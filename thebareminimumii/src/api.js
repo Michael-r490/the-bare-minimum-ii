a6019474ec267dbfc0b066fd1e4520d8e5bb058c
Michael-r490 <147607934+Michael-r490@users.noreply.github.com>
2025-03-08 01:29:44 +0000

    Working Compiler for Live

--- thebareminimumii/src/api.js
+++ thebareminimumii/src/api.js
@@ -2,13 +2,16 @@ import axios from "axios";
 
 import { LANGUAGE_VERSIONS } from "./constants";
 
+const CODE = axios.create({
+    baseURL:"https://emkc.org/api/v2/piston"
+})
 const API= axios.create({
     baseURL: import.meta.env.VITE_API_URL, 
     withCredentials: true,  
 });
 
 export const executeCode = async (language, sourceCode) =>{
-    const response = await axios.post("/execute",{
+    const response = await CODE.post("/execute",{
         language: language,
         version: LANGUAGE_VERSIONS[language],
         files: [
