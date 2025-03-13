6c9321311a00a46bb7413f0c23a35530fe3790f4
Michael-r490 <147607934+Michael-r490@users.noreply.github.com>
2025-03-13 15:20:51 +0000

    bug fix

--- thebareminimumii/src/pages/notes/TuringMachines/Note2.jsx
+++ thebareminimumii/src/pages/notes/TuringMachines/Note2.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import MainNav from '../../../components/MainNav';
+import API from '../../../api';
 
 const slides = {
     TuringMachine2: [
@@ -57,7 +58,7 @@ const TuringMachineNote2 = () => {
   const handleSlideCompletion = async () => {
     try {
       if (currentSlideIndex + 1 === totalSlides) {
-        await axios.post('https://the-bare-minimum-ii-backend.onrender.com/mark-slide-completed', {
+        await API.post('/mark-slide-completed', {
           slideId: currentSlide.id,
           sectionId: currentSection,
           isLastSlide: true,
